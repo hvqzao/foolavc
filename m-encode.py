629bf3e506c0cb6b7717a83026f8d48ddcf7de5f
hvqzao <hvqzao@gmail.com>
2021-08-23 22:34:57 +0200

    Python 3.6+ compatibility

--- m-encode.py
+++ m-encode.py
@@ -1,7 +1,7 @@
 import os,sys
 
 if len(sys.argv) == 1:
-    sys.stderr.write('Usage: {} [files...]\n'.format(sys.argv[0]))
+    sys.stderr.write(f'Usage: {sys.argv[0]} [files...]\n')
     sys.exit(1)
 
 for i in sys.argv[1:]:
@@ -10,9 +10,9 @@ for i in sys.argv[1:]:
         filename = '.'.join(i.split('.')[:-1])
         extensions = dict(exe='me', dll='ml')
         if ext in extensions.keys():
-            filename += '.{}'.format(extensions[ext])
+            filename += f'.{extensions[ext]}'
         else:
             filename += '.mf'
-        with open(filename,'wb') as f:
-            f.write(''.join(map(lambda x: '\\x{:02x}'.format(ord(x)), list(open(i,'rb').read()))))
-        print '{} saved.'.format(filename)
+        with open(filename,'w') as f:
+            f.write(''.join(map(lambda x: f'\\x{x:02x}', list(open(i,'rb').read()))))
+        print(f'{filename} saved.')
